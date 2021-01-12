d5603bb64c9b5d321b1f14e7b6a65ac4058eaddb
Daniel Vega <deveh91@gmail.com>
2021-01-12 16:57:29 -0500

    Changes according to comments

--- app/routes.js
+++ app/routes.js
@@ -3,12 +3,12 @@ const usersController = require('./controllers/users');
 const { healthCheck } = require('./controllers/healthCheck');
 const validateBySchema = require('./middlewares/validations');
 const signUpSchema = require('./schemas/user_signup');
-const uniqueEmail = require('./middlewares/uniqueEmail');
+const checkIfEmailExists = require('./middlewares/emailValidation');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/weets', weetsController.generateWeet);
-  app.post('/users', [validateBySchema(signUpSchema), uniqueEmail], usersController.signUp);
+  app.post('/users', [validateBySchema(signUpSchema), checkIfEmailExists], usersController.signUp);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
--- app/services/users.js
+++ app/services/users.js
@@ -1,14 +1,8 @@
 const db = require('../models');
 
 const createUser = body =>
-  new Promise((resolve, reject) => {
-    db.User.newUser(body)
-      .then(user => {
-        resolve(user);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
+  db.User.newUser(body)
+    .then(user => user)
+    .catch(error => error);
 
 module.exports = { createUser };
