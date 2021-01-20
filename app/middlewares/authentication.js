4bb69cb3f1b797a1c64389149f53ea70952caa58
Daniel Vega <deveh91@gmail.com>
2021-01-20 15:27:43 -0500

    Using config.common instead of directly using environment variables

--- app/middlewares/authentication.js
+++ app/middlewares/authentication.js
@@ -1,11 +1,12 @@
 const jwt = require('jwt-simple');
 const db = require('../models');
 const errors = require('../errors');
+const config = require('../../config').common.session;
 
 const verifyAuthentication = async (req, _, next) => {
   try {
     const token = req.header('Authorization');
-    const decode = await jwt.decode(token, process.env.JWT_SECRET);
+    const decode = await jwt.decode(token, config.secret);
     await db.User.findOne({ where: { id: decode.id } });
     // const user = await db.User.findOne({ where: { id: decode.id } });
     // req.user = user;
