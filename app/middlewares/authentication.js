d97e2b7d93589867355b7ee827b371efbababcd2
Daniel Vega <deveh91@gmail.com>
2021-01-20 16:46:19 -0500

    change const name in authentication middleware

--- app/middlewares/authentication.js
+++ app/middlewares/authentication.js
@@ -8,9 +8,8 @@ const config = require('../../config').common.session;
 const verifyAuthentication = async (req, _, next) => {
   try {
     const token = req.header('Authorization');
-    const decode = await jwt.decode(token, config.secret);
-    await db.User.findOne({ where: { id: decode.id } });
-    const user = await db.User.findOne({ where: { id: decode.id } });
+    const decoded = await jwt.decode(token, config.secret);
+    const user = await db.User.findOne({ where: { id: decoded.id } });
     req.user = user;
     return next();
   } catch (error) {
